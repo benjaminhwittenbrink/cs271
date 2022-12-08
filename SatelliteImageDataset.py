02e86ac35e8fb403c281b6cfa3570f7602665866
bwitten99 <witten@stanford.edu>
2022-12-08 13:22:23 -0800

    Minor bug fixes to SatImgDataset

--- SatelliteImageDataset.py
+++ SatelliteImageDataset.py
@@ -9,7 +9,7 @@ class SatelliteImageDataset(Dataset):
 
         self.root = root 
         self.image_paths = os.listdir(root)
-        self.data = pd.read_csv(csv_path)
+        self.data = pd.read_csv(csv_path + ".csv")
         self.outcome = outcome
         self.outcome_dict = dict(zip(self.data["DHSID"], self.data[outcome]))
         self.img_loader = loader
@@ -20,7 +20,7 @@ class SatelliteImageDataset(Dataset):
     def __getitem__(self, idx):
 
         image_filepath = self.root + "/" + self.image_paths[idx]
-        img = self.loader(image_filepath)
+        img = self.img_loader(image_filepath)
 
         # everything before extension 
         img_dhsid = self.image_paths[idx].split('.')[0]
